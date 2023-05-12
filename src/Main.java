8d9170986548075fbfd99588f72ff6c4f05d2490
Douglas miguel da silva <124850234+Deglas0@users.noreply.github.com>
2023-05-12 01:01:03 -0300

    update
    corrigindo entrada de dados

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         double emConta = 2000;
-        String nome = "Douglas";
-        String tipoDeConta = "Corrente";
-        Scanner operacao = new Scanner(System.in);
+        int limite = 1000;
+        System.out.println("""
+                /////////////////////////////////////////////////////////////////////////
+                
+                Digite seu nome e o tipo da sua conta:
+                
+                /////////////////////////////////////////////////////////////////////////
+                """);
+        String nome = scanner.nextLine();
+        String tipoDeConta = scanner.nextLine();
+        int operacao = 0
         String mensagem = """
                 /////////////////////////////////////////////////////////////////////////
                 Dados do Cliente: 
@@ -22,40 +31,23 @@ public class Main {
                 """.formatted(nome, tipoDeConta, emConta);
         System.out.println(mensagem);
 
-        String operacaU = operacao.nextLine();
+        do {
+            System.out.println("""
+                    Qual a operação desejada?
+                    1- Checar saldo
+                    2-
+                    3-
+                    4-sair
+                    """);
 
-            switch (operacaU) {
-                //Mostrar o extrato
-                case "1":
-                    System.out.println("""
-                            /////////////////////////////////////////////////////////////////////////
-                                                        
-                            Seu saldo atual: %f
-                                                        
-                            /////////////////////////////////////////////////////////////////////////
-                            """.formatted(emConta));
-                    break;
-                case "2":
-                    //Deposito
-                    System.out.println("""
-                            /////////////////////////////////////////////////////////////////////////
-                            
-                            
-                            
-                            /////////////////////////////////////////////////////////////////////////
-                            """);
-                    break;
-                case "3":
-                    //tranferencia
-                    System.out.println("""
-                            /////////////////////////////////////////////////////////////////////////
-                            
-                            
-                            
-                            /////////////////////////////////////////////////////////////////////////
-                            """);
-                    break;
-            }while(!operacaU.equals("10"));
+            operacao = scanner.nextInt();
+
+            if (operacao==4){
+                limite=1000;
+            }
+        }while (limite !=1000 || limite <1000 );
+
+
         }
 
     }

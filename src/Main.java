e8f558f14bef8378fdfe816e8f80b9d351e9f4f8
Douglas miguel da silva <124850234+Deglas0@users.noreply.github.com>
2023-05-14 00:32:19 -0300

    update
    adicionado envio.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double emConta = 2000;
-        int limite = 1000;
         System.out.println("""
                 /////////////////////////////////////////////////////////////////////////
                                 
@@ -15,9 +16,18 @@ public class Main {
         String nome = scanner.nextLine();
         String tipoDeConta = scanner.nextLine();
         int operacao = 0;
+        int terminal ;
+        terminal = new Random().nextInt(1000);
+
+        double taxaDeServico = 1.5;
+
+        double total ;
         String inicio = """
                 /////////////////////////////////////////////////////////////////////////
-                Dados do Cliente: 
+                
+                ** Terminal: %d **
+                
+                Dados do Cliente:
                                 
                 Nome: %s
                                 
@@ -26,13 +36,13 @@ public class Main {
                 Saldo atual: %.2f
                               
                 /////////////////////////////////////////////////////////////////////////
-                """.formatted(nome, tipoDeConta, emConta);
+                """.formatted( terminal ,nome, tipoDeConta, emConta);
 
         String opcoes = """
                     Qual a operação desejada?
                     1- Checar saldo
-                    2-
-                    3-
+                    2- TED
+                    3- ~~Receber~~
                     4-sair
                     """;
 
@@ -43,19 +53,73 @@ public class Main {
         operacao = scanner.nextInt();
 
         if (operacao==1){
-            System.out.println("""
+            System.out.printf("""
                     /////////////////////////////////////////////////////////////////////////
-                    
+                                
                     Saldo atual: %.2f
-                    
+                                
                     /////////////////////////////////////////////////////////////////////////
-                    """.formatted(emConta));
+                    %n""", emConta);
         }
         if (operacao==2){
 
+            System.out.println("""
+                    Digite o destinatario o qual vc quer efetuar a trasação:
+                    """);
+
+            scanner.nextLine();
+
+            String destinatario = scanner.nextLine();
+
+            System.out.println("""
+                    Qual valor irá enviar para o destinatário?
+                    """);
+            double valorEnviado = scanner.nextInt();
+
+
+            System.out.println("Para confirmar digite SIM");
+
+            scanner.nextLine();
+            String seg = scanner.nextLine();
+
+            if (Objects.equals(seg, "SIM") || Objects.equals(seg, "sim") || Objects.equals(seg,"Sim")) {
+                total = valorEnviado + taxaDeServico;
+                String recibo = """
+                        /////////////////////////////////////////////////////////////////////////
+                                        ATM TRANSACTION
+                                        
+                        Terminal#                 %d
+                                        
+                        Usuario:                  %s
+                                        
+                        Destinatario:             %s
+                                        
+                        Transação:                R$%.2f
+                                        
+                        Taxa de serviço:          R$%.2f
+                                        
+                        Total:                    R$%.2f
+                                        
+                        /////////////////////////////////////////////////////////////////////////
+                        """.formatted(terminal, nome, destinatario, valorEnviado, taxaDeServico, total);
+                System.out.println(recibo);
+                emConta = emConta - total;
+            }else{
+                System.out.println("Transação cancelada");
+        }
         }
         if (operacao==3){
 
+        }
+
+        if (operacao==4){
+            System.out.println("""
+                    /////////////////////////////////////////////////////////////////////////
+                    
+                    Muito obrigado por usar Nosso ATM.
+                    
+                    /////////////////////////////////////////////////////////////////////////
+                    """);
         }
         else{
             System.out.println("Opção invalida, porvafor consultar as opções possiveis");

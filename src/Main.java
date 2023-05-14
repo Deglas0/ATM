ba76051ba8713dd9a776c1e13238602948d7855c
Douglas miguel da silva <124850234+Deglas0@users.noreply.github.com>
2023-05-13 21:06:51 -0300

    opçoes
    concertando as opçes possiveis

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-Dpublic class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double emConta = 2000;
         int limite = 1000;
         System.out.println("""
                 /////////////////////////////////////////////////////////////////////////
-                
+                                
                 Digite seu nome e o tipo da sua conta:
-                
+                                
                 /////////////////////////////////////////////////////////////////////////
                 """);
         String nome = scanner.nextLine();
         String tipoDeConta = scanner.nextLine();
         int operacao = 0;
-        String mensagem = """
+        String inicio = """
                 /////////////////////////////////////////////////////////////////////////
                 Dados do Cliente: 
                                 
@@ -27,34 +27,42 @@ Dpublic class Main {
                               
                 /////////////////////////////////////////////////////////////////////////
                 """.formatted(nome, tipoDeConta, emConta);
-        System.out.println(mensagem);
 
-        do {
-            System.out.println("""
+        String opcoes = """
                     Qual a operação desejada?
                     1- Checar saldo
                     2-
                     3-
                     4-sair
-                    """);
+                    """;
 
-            operacao = scanner.nextInt();
+        System.out.println(inicio);
 
-            if (operacao==4){
-                limite=1000;
-            }
-            if (operacao==1){
-                System.out.println("""
-                /////////////////////////////////////////////////////////////////////////
-                                
-                Saldo atual: %.2f
-                              
-                /////////////////////////////////////////////////////////////////////////
-                        """.formatted(emConta));
-            }
+    while (operacao != 4){
+        System.out.println(opcoes);
+        operacao = scanner.nextInt();
 
-        }while(limite !=1000 || limite <1000);
+        if (operacao==1){
+            System.out.println("""
+                    /////////////////////////////////////////////////////////////////////////
+                    
+                    Saldo atual: %.2f
+                    
+                    /////////////////////////////////////////////////////////////////////////
+                    """.formatted(emConta));
+        }
+        if (operacao==2){
 
         }
+        if (operacao==3){
 
+        }
+        else{
+            System.out.println("Opção invalida, porvafor consultar as opções possiveis");
+        }
     }
+
+
+
+
+    }}
